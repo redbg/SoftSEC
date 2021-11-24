58a4fad9cd1cfbbb5dcb2a377da36e02da283e98
redbg <705642344@qq.com>
2021-11-24 10:23:01 +0800

    add README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# softsec
+
+## Requirements
+
+- Visual Studio Community 2019
+- Windows 10 SDK, version 2004 (10.0.19041.685)
+- Windows 10 WDK, version 2004 (10.0.19041.685)
--- build.bat
+++ build.bat
@@ -1,7 +1,9 @@
 @echo off
 
 set TargetName=softsec
-set TargetArch=x64
+set TargetArch=x86
+set TargetVersion=Win10
+set Configuration=Debug
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" %TargetArch%
 
@@ -15,17 +17,29 @@ set LIB=%LIB%;%WindowsSdkDir%Lib\%WindowsSDKVersion%km\%TargetArch%
 
 @REM Compiler Options
 set CompilerFiles=km\DriverEntry.cpp
-set CompilerOptions=/kernel /Gz /W3 /WX /Zi /std:c++17
-set Preprocessor=/D "_WIN32_WINNT=0x0A00" /D "WINVER=0x0A00" /D "WINNT=1" /D "NTDDI_VERSION=0x0A000008" /D "DBG=1"
+set CompilerOptions=/kernel /Gz /W3 /WX /std:c++17
 if %TargetArch% == x86 (
-    set Preprocessor=%Preprocessor% /D "_X86_=1" /D "i386=1" /D "STD_CALL"
+    set Preprocessor=/D "_X86_=1" /D "i386=1" /D "STD_CALL"
 )
 if %TargetArch% == x64 (
-    set Preprocessor=%Preprocessor% /D "_WIN64" /D "_AMD64_" /D "AMD64"
+    set Preprocessor=/D "_WIN64" /D "_AMD64_" /D "AMD64"
 )
+if %TargetVersion% == Win7 (
+    set Preprocessor=%Preprocessor% /D "_WIN32_WINNT=0x0601" /D "WINVER=0x0601" /D "WINNT=1" /D "NTDDI_VERSION=0x06010000"
+)
+if %TargetVersion% == Win10 (
+    set Preprocessor=%Preprocessor% /D "_WIN32_WINNT=0x0A00" /D "WINVER=0x0A00" /D "WINNT=1" /D "NTDDI_VERSION=0x0A000008"
+)
+
 @REM Linker Options
-set LinkerOptions=/Driver /ENTRY:"DriverEntry" /SUBSYSTEM:NATIVE /NODEFAULTLIB /DEBUG /OUT:%TargetName%_%TargetArch%.sys
+set LinkerOptions=/Driver /ENTRY:"DriverEntry" /SUBSYSTEM:NATIVE,"6.01" /NODEFAULTLIB /OUT:%TargetName%_%TargetArch%.sys
 set LinkerLibrary="ntoskrnl.lib"
+
+if %Configuration% == Debug (
+    set CompilerOptions=%CompilerOptions% /Zi
+    set Preprocessor=%Preprocessor% /D "DBG=1"
+    set LinkerOptions=%LinkerOptions% /DEBUG
+)
 
 @REM Compiler & Linker
 cl %CompilerFiles% %CompilerOptions% %Preprocessor% /link %LinkerOptions% %LinkerLibrary%
